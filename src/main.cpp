f83a4dd387147e1277630f102f5cb93835f67020
ewyrick-mark-1 <elliot@wyrick.ws>
2025-09-30 10:46:29 -0400

    setting up project. wip

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<array<int, 2>> invalid_rows;
+vector<array<int, 2>> invalid_cols;
+vector<array<int, 2>> invalid_pos_diags;
+vector<array<int, 2>> invalid_neg_diags;
+
+int main(){
+    int col_size, row_size, queen_size;
+    
+    cout<<"enter number of columns:\n";
+    cin>>col_size;
+
+    cout<<"enter number of rows:\n";
+    cin>>row_size;
+
+    cout<<"enter number of queens:\n";
+    cin>>queen_size;
+
+    
+    return 0;
+}
+
+bool inSet(){
+    
+}
