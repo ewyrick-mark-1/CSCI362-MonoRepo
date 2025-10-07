6cf98f5e14a1c8941e92e823b190b528bcf513ff
ewyrick-mark-1 <elliot@wyrick.ws>
2025-10-07 13:56:59 -0400

    complete

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 Basic integer selection sort program.
-Elliot Wyrick - 9/9/25
+Elliot Wyrick - 9/29/25
 
 To run:
 
-- initialize CMake in project directory         "cmake ."
+- initialize CMake in project directory         "cmake .."
 - run the makefile it creates                   "make"
-- run the executable made by the makefile       "./SelectionSort"
+- run the executable made by the makefile       "./NQueens"
 
-terminal will prompt for:
-- size of the array to be sorted
-- each individual array element
-
-upon entering all data, a confirmation of the array you entered will be printed, as well as the sorted array.
+terminal will prompt for number of queens.
+prints all valid solutions with board visualization.
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <stack>
+#include <array>
 
 using namespace std;
 
-vector<array<int, 2>> invalid_rows;
-vector<array<int, 2>> invalid_cols;
-vector<array<int, 2>> invalid_pos_diags;
-vector<array<int, 2>> invalid_neg_diags;
+int SIZE = 0;
 
-int main(){
-    int col_size, row_size, queen_size;
-    
-    cout<<"enter number of columns:\n";
-    cin>>col_size;
-
-    cout<<"enter number of rows:\n";
-    cin>>row_size;
-
-    cout<<"enter number of queens:\n";
-    cin>>queen_size;
-
-    
-    return 0;
+bool isValid(int row, int col, stack<array<int, 2>> stk){
+    while(!stk.empty()){
+        int qRow = stk.top()[0];
+        int qCol = stk.top()[1];
+        stk.pop();
+        if(qCol == -1) continue; // skip dummy entries
+        if(row == qRow || col == qCol || abs(row - qRow) == abs(col - qCol)){
+            return false;
+        }
+    }
+    return true;
 }
 
-bool inSet(){
+int main(){
+    cout << "enter number of queens (an integer between 4 and 10):\n";
+    cin >> SIZE;
+    while(SIZE < 4 || SIZE > 10){
+        cout << "Invalid input. Please enter a number between 4 and 10:\n";
+        cin >> SIZE;
+    }
+
+    stack <array<int, 2>> queenStack; // [row, col], [row, col]
+    queenStack.push({0, -1});
+    int solution_counter = 0;
+
+    while(!queenStack.empty()){
+        int row = queenStack.top()[0];
+        int col = queenStack.top()[1];
+        queenStack.pop();
+
+        if(row == SIZE){
+            //yay
+            solution_counter++;
+            cout << "solution " <<solution_counter<< " found\n";
+            stack<array<int,2>> temp = queenStack;  // copy
+            vector<array<int,2>> solution;
+            while(!temp.empty()){
+                solution.push_back(temp.top());
+                temp.pop();
+            }
+            for(int r = solution.size()-1; r >= 0; r--){
+                for(int n = 0; n < SIZE; n++){
+                    if(n == solution[r][1]){
+                        cout << " Q";
+                    }else{
+                        cout <<" .";
+                    }
+                }
+                cout << '\n';
+            }
+            cout << '\n';
+            continue;
+        }
+
+        for(int i = col+1; i < SIZE; i++){
+            if(isValid(row, i, queenStack)){
+                queenStack.push({row, i}); // push current valid column
+                queenStack.push({row + 1, -1}); // push next row
+                break;
+            }
+        }
+    }
     
+    return 0;
 }
